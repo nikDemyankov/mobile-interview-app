cadb77670150a55ab4d6408ece8654450972080d
dansjo <daniel.sjostrom@nordnet.se>
2021-08-16 15:00:16 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- src/screen/instrumentList/InstrumentListScreen.tsx
+++ src/screen/instrumentList/InstrumentListScreen.tsx
@@ -1,13 +1,26 @@
 import React, { memo } from 'react';
-import { Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { FlatList, Text, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 import { Screen } from '../../component';
+import { winnersLosers } from '../../ducks';
 
-const InstrumentListScreen: React.FC = () => (
-  <Screen>
-    <View>
-      <Text>Welcome</Text>
-    </View>
-  </Screen>
-);
+const InstrumentListScreen: React.FC = () => {
+  const dispatch = useDispatch();
+  const winners = useSelector(winnersLosers.selector.getWinners);
+
+  useEffect(() => {
+    dispatch(winnersLosers.action.getWinners());
+  }, [dispatch]);
+
+  return (
+    <Screen>
+      <View>
+        <Text>Welcome</Text>
+      </View>
+      <FlatList data={winners} renderItem={data => <Text>{data.item.name}</Text>} />
+    </Screen>
+  );
+};
 
 export default memo(InstrumentListScreen);
